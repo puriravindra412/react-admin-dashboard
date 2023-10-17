45ed2228120e5088607dd22caac436f06b6cef80
puriravindra412 <puriravindra412@gmail.com>
2023-10-17 23:45:14 +0530

    new  commit

--- src/components/MuiDrawer.jsx
+++ src/components/MuiDrawer.jsx
@@ -12,14 +12,14 @@ const MuiDrawer = ()=>{
   
 
   const list = (
-    <Box  classNamerole="presentation">
+    <Box  className="h-screen" role="presentation">
       <SideBar show={true}/>
       
     </Box>
   );
 
   return (
-    <div>
+    <div className=''>
       < >
      
     <button className="inline-flex items-center p-2 m-2 ml-3 text-sm text-gray-500 rounded-lg h-12 w-12 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " onClick={()=>setOpen(true)}>
